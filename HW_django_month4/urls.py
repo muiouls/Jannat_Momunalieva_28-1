3bfde3b0ce30e7f0fdda4d941736d67ff003704d
muiouls <Jaka.Momunalieva07@mail.ru>
2023-05-27 00:03:48 +0600

    hw6

--- HW_django_month4/urls.py
+++ HW_django_month4/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from posts.views import main_view, products_view, product_detail_view, product_create_view
 from HW_django_month4 import settings
 
+from users.views import register_view, login_view, logout_view
 
 """
 
@@ -39,6 +40,10 @@ urlpatterns = [
     path('products/', products_view),
     path('products/<int:id>/', product_detail_view),
     path('products/create/', product_create_view),
+
+    path('users/register/', register_view),
+    path('users/login/', login_view),
+    path('users/logout/', logout_view),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
